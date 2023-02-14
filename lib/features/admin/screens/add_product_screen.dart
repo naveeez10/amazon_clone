f0e3b94376607cd64d69f35e1746d4744a78069b
Khoja Naviz <20itnav068@ldce.ac.in>
2023-02-14 22:52:04 +0530

    implementing adding product feature

--- lib/features/admin/screens/add_product_screen.dart
+++ lib/features/admin/screens/add_product_screen.dart
@@ -1,7 +1,11 @@
+import 'dart:io';
+
 import 'package:amazon_clone/common/widgets/custom_text_field.dart';
+import 'package:amazon_clone/constants/utils.dart';
 import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
-
+import '';
+import '../../../common/widgets/custom_button.dart';
 import '../../../constants/global_variables.dart';
 
 class AddProduct extends StatefulWidget {
@@ -18,6 +22,9 @@ class _AddProductState extends State<AddProduct> {
   final TextEditingController priceController = TextEditingController();
   final TextEditingController quantityController = TextEditingController();
 
+  String category = "Mobiles";
+  List<File> images = [];
+
   @override
   void dispose() {
     super.dispose();
@@ -25,6 +32,21 @@ class _AddProductState extends State<AddProduct> {
     descriptionController.dispose();
     priceController.dispose();
     quantityController.dispose();
+  }
+
+  List<String> productCategories = [
+    'Mobiles',
+    'Essentials',
+    'Appliances',
+    'Books',
+    'Fashion',
+  ];
+
+  void selectImages() async {
+    var res = await pickImages();
+    setState(() {
+      images = res;
+    });
   }
 
   @override
@@ -53,36 +75,39 @@ class _AddProductState extends State<AddProduct> {
                 const SizedBox(
                   height: 10,
                 ),
-                DottedBorder(
-                  borderType: BorderType.RRect,
-                  radius: const Radius.circular(10),
-                  dashPattern: const [10, 4],
-                  strokeCap: StrokeCap.round,
-                  child: Container(
-                    width: double.infinity,
-                    height: 150,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        const Icon(
-                          Icons.folder_open,
-                          size: 40,
-                        ),
-                        const SizedBox(
-                          height: 15,
-                        ),
-                        Text(
-                          'Select Product Images',
-                          style: TextStyle(
-                            fontSize: 15,
-                            color: Colors.grey.shade400,
-                            fontWeight: FontWeight.w600,
+                GestureDetector(
+                  onTap: selectImages,
+                  child: DottedBorder(
+                    borderType: BorderType.RRect,
+                    radius: const Radius.circular(10),
+                    dashPattern: const [10, 4],
+                    strokeCap: StrokeCap.round,
+                    child: Container(
+                      width: double.infinity,
+                      height: 150,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Icon(
+                            Icons.folder_open,
+                            size: 40,
                           ),
-                        )
-                      ],
+                          const SizedBox(
+                            height: 15,
+                          ),
+                          Text(
+                            'Select Product Images',
+                            style: TextStyle(
+                              fontSize: 15,
+                              color: Colors.grey.shade400,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          )
+                        ],
+                      ),
                     ),
                   ),
                 ),
@@ -97,12 +122,49 @@ class _AddProductState extends State<AddProduct> {
                   height: 8,
                 ),
                 CustomTextField(
-                  controller: productNameController,
-                  hintText: 'Product Description',
+                  controller: descriptionController,
+                  hintText: 'Description',
+                  maxLines: 7,
                 ),
                 const SizedBox(
-                  height: 30,
+                  height: 8,
                 ),
+                CustomTextField(
+                  controller: priceController,
+                  hintText: 'Price',
+                ),
+                const SizedBox(
+                  height: 8,
+                ),
+                CustomTextField(
+                  controller: quantityController,
+                  hintText: 'Quantity',
+                ),
+                const SizedBox(
+                  height: 8,
+                ),
+                SizedBox(
+                  width: double.infinity,
+                  child: DropdownButton(
+                    value: category,
+                    icon: const Icon(Icons.keyboard_arrow_down),
+                    items: productCategories.map((String item) {
+                      return DropdownMenuItem(
+                        child: Text(item),
+                        value: item,
+                      );
+                    }).toList(),
+                    onChanged: (String? newVal) {
+                      setState(() {
+                        category = newVal!;
+                      });
+                    },
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                CustomButton(text: 'Sell', onTap: () {}),
               ],
             ),
           ),
